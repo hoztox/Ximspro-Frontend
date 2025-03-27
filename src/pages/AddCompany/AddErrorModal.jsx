d96b5ea9010e14d68287fbf947da45607aabc354
afeefnk <connectafeefnk@gmail.com>
2025-03-27 17:01:00 +0530

    Update in Add Company Success Modal

--- src/pages/AddCompany/AddErrorModal.jsx
+++ src/pages/AddCompany/AddErrorModal.jsx
@@ -12,24 +12,24 @@ const AddErrorModal = ({ showAddErrorModal, onClose }) => {
       <AnimatePresence>
         {showAddErrorModal && (
           <motion.div
-            className="modal-overlay"
+            className="add-cmy-error-modal-overlay"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
           >
             <motion.div
-              className={`modal ${theme === "dark" ? "dark" : "light"}`}
+              className={`add-cmy-error-modal ${theme === "dark" ? "dark" : "light"}`}
               initial={{ scale: 0.9 }}
               animate={{ scale: 1 }}
               exit={{ scale: 0.9 }}
               transition={{ duration: 0.3 }}
             >
-              <div className={`modal-content flex flex-col items-center justify-center ${theme === "dark" ? "dark" : "light"}`}>
+              <div className={`add-cmy-error-modal-content flex flex-col items-center justify-center ${theme === "dark" ? "dark" : "light"}`}>
                 <img src={errors} alt="" className="w-12" />
-                <h1 className="messegehead">An error occurred!</h1>
-                <p className="messege">Please try again</p>
-                <button onClick={onClose} className="deleteclosebtns duration-200">
+                <h1 className="add-cmy-error-messegehead">An error occurred!</h1>
+                <p className="add-cmy-error-messege">Please try again</p>
+                <button onClick={onClose} className="add-cmy-error-deleteclosebtns duration-200">
                   Close
                 </button>
               </div>
