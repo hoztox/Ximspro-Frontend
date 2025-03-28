82d4d7c9308e11047cd4e013cf28f1a61d04cfa4
afeefnk <connectafeefnk@gmail.com>
2025-03-28 12:18:07 +0530

    update the delete modals

--- src/pages/Companies/DeleteSucessModal.jsx
+++ src/pages/Companies/DeleteSucessModal.jsx
@@ -1,37 +1,42 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import check from "../../assets/images/Modal/success.svg";
 import "./deletesucessmodal.css";
-import { useTheme } from "../../ThemeContext";
+import { motion, AnimatePresence } from "framer-motion";
+import success from "../../assets/images/Modal/successIllustration.svg";
+import successdark from "../../assets/images/Modal/successIllustrationdark.svg";
+import { useTheme } from '../../ThemeContext';
+import { X } from "lucide-react";
 
 const DeleteSucessModal = ({ showDeleteSuccessModal, onClose }) => {
-      const { theme } = useTheme();
+  const { theme } = useTheme();
 
   if (!showDeleteSuccessModal) return null;
   return (
     <AnimatePresence>
       {showDeleteSuccessModal && (
         <motion.div
-          className="modal-overlay"
+          className="success-modal-overlay"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
         >
           <motion.div
-            className={`modal ${theme === "dark" ? "dark" : "light"}`}
+            className={`success-modal ${theme === "dark" ? "dark" : "light"}`}
             initial={{ scale: 0.9 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0.9 }}
             transition={{ duration: 0.3 }}
           >
-            <div className={`modal-content flex flex-col items-center justify-center ${theme === "dark" ? "dark" : "light"}`}>
-              <img src={check} alt="" className="w-12" />
-              <h1 className="messegehead">Success!</h1>
-              <p className="messege">Company Deleted Successfully!</p>
-              <button onClick={onClose} className="successclosebtn duration-200">
-                Close
+            <div className='flex justify-end'>
+              <button onClick={onClose} className="bg-[#eeeeee] rounded-md w-[36px] h-[36px] flex justify-center items-center modal-close-icon">
+                <X className='w-[24px] h-[24px] text-[#1C1C24] close-img' />
               </button>
+            </div>
+            <div className={`success-modal-content flex flex-col items-center justify-center ${theme === "dark" ? "dark" : "light"}`}>
+              <img src={success} alt="" className="w-[144px] h-[144px] success-light" />
+              <img src={successdark} alt="" className="w-[144px] h-[144px] success-dark" />
+              <h1 className="success-messegehead">Success!!</h1>
+              <p className="success-messege">Company Deleted Successfully</p>
             </div>
           </motion.div>
         </motion.div>
